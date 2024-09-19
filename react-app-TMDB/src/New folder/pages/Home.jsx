75466fc7a354d921e5249f5f326387f49a9fb35a
AppleCraft <jovangilbert737@gmail.com>
2024-09-19 11:40:30 +0800

    menambahkan prohrct bary

---
+++ react-app-TMDB/src/New folder/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Schematic Movie Databases</h1>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default Home;
---
+++ react-app-TMDB/src/New folder/pages/Results.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+import MovieList from '../components/MovieList';
+
+const API_KEY = '55459cf8dec27b4442e97f5d79b2e662';
+
+const Results = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get('query');
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`
+        );
+        setMovies(response.data.results);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+        setLoading(false);
+      }
+    };
+
+    if (query) {
+      fetchMovies();
+    }
+  }, [query]);
+
+  return (
+    <div>
+      <h2>Search Results for "{query}"</h2>
+      {loading ? <p>Loading...</p> : <MovieList movies={movies} />}
+    </div>
+  );
+};
+
+export default Results;
