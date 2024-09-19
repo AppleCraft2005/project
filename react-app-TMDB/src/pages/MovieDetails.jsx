89ec30390f65414ad0f4ebf50dcdffeedc8d0e49
Putra Whyra Pratama S. <putragaul789@gmail.com>
2024-09-19 18:44:46 +0700

    Add genre in MovieDetails.jsx

--- react-app-TMDB/src/pages/MovieDetails.jsx
+++ react-app-TMDB/src/pages/MovieDetails.jsx
@@ -7,24 +7,45 @@ const API_KEY = '55459cf8dec27b4442e97f5d79b2e662';
 const MovieDetails = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
+  const [genres, setGenres] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
       try {
-        const response = await axios.get(
+        const movieResponse = await axios.get(
           `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
         );
-        setMovie(response.data);
-        setLoading(false);
+        setMovie(movieResponse.data);
       } catch (error) {
         console.error('Error fetching movie details:', error);
-        setLoading(false);
+      }
+    };
+
+    const fetchGenres = async () => {
+      try {
+        const genresResponse = await axios.get(
+          `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`
+        );
+        setGenres(genresResponse.data.genres);
+      } catch (error) {
+        console.error('Error fetching genres:', error);
       }
     };
 
     fetchMovieDetails();
+    fetchGenres();
+    setLoading(false);
   }, [id]);
+
+  const getGenreNames = (genreIds) => {
+    if (!genreIds || genres.length === 0) return [];
+
+    return genreIds.map((id) => {
+      const genre = genres.find((genre) => genre.id === id);
+      return genre ? genre.name : '';
+    });
+  };
 
   if (loading) {
     return <p>Loading...</p>;
@@ -34,12 +55,15 @@ const MovieDetails = () => {
     return <p>Movie not found</p>;
   }
 
+  const genreNames = getGenreNames(movie.genres.map((genre) => genre.id));
+
   return (
     <div>
       <h2>{movie.title}</h2>
       <p>{movie.overview}</p>
       <p>Release Date: {movie.release_date}</p>
       <p>Rating: {movie.vote_average}</p>
+      <p>Genres: {genreNames.join(', ')}</p> {/* Display the genres */}
     </div>
   );
 };
