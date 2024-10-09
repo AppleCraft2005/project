9c6cffaafca1e87c0951cc9b19a72191a08998da
Putra Whyra Pratama S. <putragaul789@gmail.com>
2024-10-09 21:26:43 +0700

    Memperbaiki leaderboard

--- web-design-UNESA/src/App.jsx
+++ web-design-UNESA/src/App.jsx
@@ -1,27 +1,41 @@
+import React, { useState } from 'react';
 import Header from './components/ui/Header'
 import Footer from './components/ui/Footer'
 import Main from './components/ui/Main'
-import ChallengeList from './components/ChallengeList'
+//import ChallengeList from './components/ChallengeList' # Tidak butuh karena sudah diwakilkan oleh Challenges
 import Challenges from './components/Challenges'
 import Leaderboard from './components/Leaderboard'
 import Community from './components/Community'
 import Resources from './components/Resources'
 
 function App() {
+  const [users, setUsers] = useState([
+    { name: 'You', points: 0 },
+    { name: 'Jovan', points: 120 },
+    { name: 'Putra', points: 100 },
+    { name: 'Abby', points: 80 },
+  ]);
+
+  const handleCompleteChallenge = (earnedPoints) => {
+    setUsers((prevUsers) =>
+      prevUsers.map((user) =>
+        user.name === 'You' ? { ...user, points: user.points + earnedPoints } : user
+      )
+    );
+  };
 
   return (
-    <div  >
+    <div className="App">
       <Header></Header>
       <Main />
-      {/* <ChallengeList></ChallengeList> */}
-      <Challenges></Challenges>
-      {/* <Leaderboard></Leaderboard> */}
+      <Challenges onCompleteChallenge={handleCompleteChallenge} />
+      <Leaderboard users={users} />
       <Community></Community>
-      {/* <Resources></Resources> */}
+      <Resources></Resources>
       <Footer />
     </div>
       
   )
 }
 
-export default App
+export default App;
